a526e650063c542dc0f2cea75fe25a83d7665a32
Hackbright Student <no-reply@hackbrightacademy.com>
2017-08-23 16:39:06 -0700

    finalized calculator

--- calculator.py
+++ calculator.py
@@ -6,6 +6,38 @@ calculator program yourself in this file.
 
 from arithmetic import *
 
+
+def find_opp(opp):
+    if opp == "+":
+        return add
+    elif opp == "-":
+        return subtract
+    elif opp == "*":
+        return multiply
+    elif opp == "/":
+        return divide
+    elif opp == "square":
+        return square
+    elif opp == "cube":
+        return cube
+    elif opp == "pow":
+        return power
+    elif opp == "mod":
+        return mod
+
+
+def calc_3(par):
+    """runs a arthemtic opperation on two numbers"""
+
+    return find_opp(par[0])(int(par[1]), int(par[2]))
+
+
+def calc_2(par):
+    """runs arthemtic opperation when only one number"""
+
+    return find_opp(par[0])(int(par[1]))
+
+
 while True:
     input_string = raw_input(">")
     t_input = input_string.split(" ")
@@ -14,16 +46,10 @@ while True:
         break
 
     else:
-        opp = t_input[0]
-        if opp == "+":
-            print add(int(t_input[1]), int(t_input[2]))
-        elif opp == "-":
-            print subtract(int(t_input[1]), int(t_input[2]))
-        elif opp == "*":
-            print multiply(int(t_input[1]), int(t_input[2]))
-        elif opp == "/":
-            print divide(int(t_input[1]), int(t_input[2]))
-
+        if len(t_input) < 3:
+            print calc_2(t_input)
+        else:
+            print calc_3(t_input)
 
 
 
