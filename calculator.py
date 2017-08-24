04351c800034fba249249ab2c570de579c3a7d27
Hackbright Student <no-reply@hackbrightacademy.com>
2017-08-24 13:04:36 -0700

    Changed to use Reduce function - to work with arithmetic.py and added new float function

--- calculator.py
+++ calculator.py
@@ -4,7 +4,7 @@ Using the arithmetic.py file from Calculator Part 1, create the
 calculator program yourself in this file.
 """
 
-from arithmetic1 import *
+from arithmetic import *
 
 
 def find_opp(opp):
@@ -26,27 +26,30 @@ def find_opp(opp):
         return mod
 
 
-def calc(par):
+def calc(opperator, numbers):
     """runs a arthemtic opperation on list of numbers (of any size)"""
 
-    if find_opp(par[0]) is None:
+    if find_opp(opperator) is None:
         return "Not a valid operator"
     else:
-        return find_opp(par[0])(par[1:])
+        return reduce(find_opp(opperator), numbers)
 
 #def calc_2(par):
 #    """runs arthemtic opperation when only one number"""
 #
 #    return find_opp(par[0])(int(par[1]))
 
-#def flo_inputs(num_list):
-#    """ chaneges a list in to a list of floats"""
-#
-#    try:
-#       for i in num_list:
-#
+
+def flo_inputs(num_list):
+    """ chaneges a list in to a list of floats"""
+
+    flo_list = []
+
+    for i in num_list:
+        flo_list.append(float(i))
+    return flo_list
 #   except ValueError:
-#        print "Only numbers after operator accepted"
+ #       print "Only numbers after operator accepted"
 
 while True:
     input_string = raw_input(">")
@@ -55,12 +58,13 @@ while True:
         print "You are now exiting!"
         break
     else:
+        opp = t_input[0]
         try:
-            float(t_input[1])
+            num_list = flo_inputs(t_input[1:])
         except ValueError:
             print "only numbers after operator accepted"
             continue
-        print calc(t_input)
+        print calc(opp, num_list)
         #if len(t_input) < 3:
         #    print calc_2(t_input)
         #else:
